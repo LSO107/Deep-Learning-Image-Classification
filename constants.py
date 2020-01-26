d5f0fb81e49748619c0146c057600beda2767b30
LSO107 <34274877+LSO107@users.noreply.github.com>
2020-01-26 20:45:40 +0000

    Add constants class
    Class to hold figures, saves changing them everywhere in classes if the image size etc ever changes

---
+++ constants.py
@@ -0,0 +1,20 @@
+# Ratio for splitting dataset
+train_ratio = 0.75
+val_ratio = 0.15
+test_ratio = 0.10
+
+# Output classes
+class_names = ["Uninfected", "Infected"]
+
+# Paths for images & labels
+cells_path = 'Data/Cells.npy'
+labels_path = 'Data/Labels.npy'
+
+# Image size
+image_size = 64, 64
+
+# Input shape for images
+input_shape = 64, 64, 3
+
+# Reshape size for numpy
+reshape_size = [1, 64, 64, 3]
